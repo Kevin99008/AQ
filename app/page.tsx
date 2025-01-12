4ad4df28160930c8de2471c3b2037977fd6dbec8
Kevin99008 <salute.kevin99.com>
2025-01-12 17:26:23 +0700

    modify infoCard, add imagePreview component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import classes from "./page.module.css"
+import ImageBackground from "@/components/images/imageBackground";
 import Card from "@/components/InfoCard/card";
-import Image from "next/image";
+
 import aquakidsLogo from "@/assets/logo.png";
 import playsoundLogo from "@/assets/logo2.png";
-import ImageBackground from "@/components/images/imageBackground";
+import AQImg1 from "@/assets/aquaKids/1.jpg";
+import AQImg2 from "@/assets/aquaKids/2.jpg";
+import AQImg3 from "@/assets/aquaKids/3.jpg";
+import AQImg4 from "@/assets/aquaKids/4.jpg";
+import PSImg1 from "@/assets/playsound/1.jpg";
+import PSImg2 from "@/assets/playsound/2.jpg";
+import PSImg3 from "@/assets/playsound/3.jpg";
+import PSImg4 from "@/assets/playsound/4.jpg";
 
 export default function Home() {
 
@@ -15,7 +23,13 @@ export default function Home() {
       "Engaging Learning Environment: We utilize songs, games, and water toys to create an interactive experience that encourages children to learn while having fun. This approach not only builds swimming skills but also promotes physical and emotional development.",
       "Expert Instructors: Our certified instructors are passionate about teaching and are committed to ensuring every child feels confident and safe in the water."
     ],
-    logo: aquakidsLogo
+    logo: aquakidsLogo,
+    previewImage: [
+      AQImg1.src,
+      AQImg2.src,
+      AQImg3.src,
+      AQImg4.src
+    ]
   }
   
   const playsoundData = {
@@ -26,23 +40,27 @@ export default function Home() {
       "Cognitive Development: Engaging with music enhances children's IQ and cognitive skills, making it a vital part of their early education.",
       "Fun Learning Environment: We believe that learning music should be enjoyable! Our interactive classes keep children excited and motivated to learn."
     ],
-    logo: playsoundLogo
+    logo: playsoundLogo,
+    previewImage: [
+      PSImg1.src,
+      PSImg2.src,
+      PSImg3.src,
+      PSImg4.src
+    ]
   }
 
   return (
     <>
       <div className={classes.containerImage}>
         <span className={classes.text}>AquaCube</span>
-        <div className={classes.subText}>Webpage for Aquakids & Playsound Chiangmai</div>
+        <div className={classes.subText}>Webprovider for Aquakids & Playsound Chiangmai</div>
         <ImageBackground />
       </div>
       <div className={classes.container}>
-        <Image src={aquakidsData.logo.src} alt={aquakidsData.title} width={400} height={400}/>
-        <Card title={aquakidsData.title} detail={aquakidsData.detail} description={aquakidsData.description}/>
+        <Card title={aquakidsData.title} detail={aquakidsData.detail} description={aquakidsData.description} logo={aquakidsData.logo.src} previewImage={aquakidsData.previewImage}/>
       </div>
       <div className={classes.container}>
-        <Image src={playsoundData.logo.src} alt={aquakidsData.title} width={400} height={400}/>
-        <Card title={playsoundData.title} detail={playsoundData.detail} description={playsoundData.description}/>
+        <Card title={playsoundData.title} detail={playsoundData.detail} description={playsoundData.description} logo={playsoundData.logo.src} previewImage={playsoundData.previewImage}/>
       </div>
     </>
 );
