50d967a8220005a5806141ccc61cdd9b2cbde35d
ingfosbreak <ingfosbreak@outlook.com>
2025-03-22 21:27:17 +0700

    fix: disable eslint

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: process.env.NEXT_NO_ESLINT === 'true',
+  },
   reactStrictMode: false, // Disable Strict Mode
   images: {
     domains: ["irrgwghpvvwcgalcfkmh.supabase.co", "sryesoktrkolkclrolcf.supabase.co"], // Add your Supabase storage domain
